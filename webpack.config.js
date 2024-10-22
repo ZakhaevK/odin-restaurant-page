49b57d5197477e98c1ec6e7b0909e8b0021905e2
ZakhaevK <zkupke@gmail.com>
2024-10-22 18:43:19 +1100

    Add index.js and webpack.config.js Currently is using script from previous lesson, but it is functional. This includes steps up to 4 on the project page.

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+// webpack.config.js
+/* 
+This is a Webpack configuration file that contains all details
+needed for bundling, like entry point, output destination, and 
+anything like plugins and loaders.
+*/
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"), // Output directory, in this case, dist
+    clean: true, // Empties output directory each time webpack is run to bundle
+  },
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./src/template.html"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html"
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
