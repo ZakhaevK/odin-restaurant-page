fb53812c0d8dbb3708a084e8ae34fe12c92f7289
ZakhaevK <zkupke@gmail.com>
2024-10-23 18:15:44 +1100

    Add content.js as a module for import All steps up to 6 are complete at this point.

---
+++ src/content.js
@@ -0,0 +1,17 @@
+import image from "./images/club-sw.jpg";
+
+const contentDiv = document.getElementById("content");
+
+export const generateHome = function() {
+  const img = document.createElement("img");
+  const h2 = document.createElement("h2");
+  const p = document.createElement("p");
+
+  img.src = image;
+  h2.textContent = "Loremstaurant";
+  p.textContent = "This restaurant is fantastic, and you will enjoy it greatly";
+
+  contentDiv.appendChild(img);
+  contentDiv.appendChild(h2);
+  contentDiv.appendChild(p);
+}
--- src/index.js
+++ src/index.js
@@ -1,19 +1,4 @@
-import image from "./images/club-sw.jpg";
+import { generateHome } from "./content";
 
-const contentDiv = document.getElementById("content");
-
-
-const img = document.createElement("img");
-const h2 = document.createElement("h2");
-const p = document.createElement("p");
-
-img.src = image;
-h2.textContent = "Loremstaurant";
-p.textContent = "This restaurant is fantastic, and you will enjoy it greatly"
-
-contentDiv.appendChild(img);
-contentDiv.appendChild(h2);
-contentDiv.appendChild(p);
-
-
+generateHome();
 console.log("Hello world!");
