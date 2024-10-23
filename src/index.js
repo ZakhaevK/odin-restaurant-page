c6551d5ed6e054ab2fcc6bbedce6b85ac3106712
ZakhaevK <zkupke@gmail.com>
2024-10-23 17:49:52 +1100

    Move element to JS file. Still needs to be moved to own module and imported into index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
+import image from "./images/club-sw.jpg";
+
+const contentDiv = document.getElementById("content");
+
+
+const img = document.createElement("img");
+const h2 = document.createElement("h2");
+const p = document.createElement("p");
+
+img.src = image;
+h2.textContent = "Loremstaurant";
+p.textContent = "This restaurant is fantastic, and you will enjoy it greatly"
+
+contentDiv.appendChild(img);
+contentDiv.appendChild(h2);
+contentDiv.appendChild(p);
+
+
 console.log("Hello world!");
