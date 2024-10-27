f599c837dc4de740a173ca4f70b21e265483b1a9
ZakhaevK <zkupke@gmail.com>
2024-10-27 16:20:27 +1100

    Add generate and cleanup functions - Includes both index and content files. - Not tested yet.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,28 @@
-import { generateHome } from "./content";
+import { generateHome, generateMenu, generateAbout } from "./content";
+
+const homeButt = document.getElementById("home");
+const menuButt = document.getElementById("menu");
+const aboutButt = document.getElementById("about");
+const contentDiv = document.getElementById("content");
+
+homeButt.addEventListener(click, () => {
+  wipeContent()
+  generateHome();
+})
+
+menuButt.addEventListener(click, () => {
+  wipeContent()
+  generateMenu();
+})
+
+aboutButt.addEventListener(click, () => {
+  wipeContent()
+  generateAbout();
+})
+
+function wipeContent() {
+  contentDiv.replaceChildren();
+}
 
 generateHome();
 console.log("Hello world!");
